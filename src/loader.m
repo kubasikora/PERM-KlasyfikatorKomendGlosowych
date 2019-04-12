d3674ad9f9576a37997a1ab0c12e4754dedf5a08
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-04-12 18:35:55 +0200

    stworzone pliki z srednimi charakterystykami komend

--- src/loader.m
+++ src/loader.m
@@ -1,12 +1,23 @@
 clear all
-[command1, Fs1] = obcinator('../Przerobione/01-Kamil1.wav');
-[command2, Fs2] = obcinator('../Przerobione/01-Kamil2.wav');
-[command3, Fs3] = obcinator('../Przerobione/01-Kuba1.wav');
-[command4, Fs4] = obcinator('../Przerobione/01-Kuba2.wav');
+
+COMMAND_NUM = 1;
+
+[command1, Fs1] = obcinator(strcat('../Przerobione/0', num2str(COMMAND_NUM), '-Kamil1.wav'));
+[command2, Fs2] = obcinator(strcat('../Przerobione/0', num2str(COMMAND_NUM), '-Kamil2.wav'));
+[command3, Fs3] = obcinator(strcat('../Przerobione/0', num2str(COMMAND_NUM), '-Kuba1.wav'));
+[command4, Fs4] = obcinator(strcat('../Przerobione/0', num2str(COMMAND_NUM), '-Kuba2.wav'));
 
 WINDOW_NUM = 32;
 
 commands = {command1 command2 command3 command4};
+
+sum_cog = zeros(WINDOW_NUM,1);
+sum_mean = zeros(WINDOW_NUM,1);
+sum_dev = zeros(WINDOW_NUM,1);
+
+center_of_gravity = zeros(WINDOW_NUM,1);
+means = zeros(WINDOW_NUM, 1);
+deviations = zeros(WINDOW_NUM, 1);
 
 for cell=commands
     command=cell{1};
@@ -14,30 +25,46 @@ for cell=commands
 
     ft = linspace(0,Fs4, length(F))';
 
-    center_of_gravity = zeros(WINDOW_NUM,1);
+    
     for i=1:WINDOW_NUM
         center_of_gravity(i) = eval_COG(ft, abs(S(:,i))); 
-    end
+        means(i) = mean(abs(S(:,i)));
+        deviations(i) = std(abs(S(:,i)));
+    end 
     
-    means = zeros(WINDOW_NUM, 1);
-    for i=1:WINDOW_NUM
-       means(i) = mean(abs(S(:,i)));
-    end
-    
-    figure(1)
-    title("Środki ciężkości komendy");
-    hold on
-    grid on
-    grid minor
-    plot(linspace(1,WINDOW_NUM, WINDOW_NUM), center_of_gravity);
-    hold off
-    
-    figure(2)
-    title("Średnia częstotliwość w danym oknie");
-    hold on
-    grid on
-    grid minor
-    plot(linspace(1,WINDOW_NUM, WINDOW_NUM), means);
-       
+    sum_cog = sum_cog + center_of_gravity;
+    sum_mean = sum_mean + means;
+    sum_dev = sum_dev + deviations;
 end
+
+sum_cog = sum_cog/4;
+sum_mean = sum_mean/4;
+sum_dev = sum_dev/4;
+
+figure(1)
+title("Średni środek ciężkości komendy");
+hold on
+grid on
+grid minor
+plot(linspace(1,WINDOW_NUM, WINDOW_NUM), sum_cog);
+hold off
+
+figure(2)
+title("Średnia częstotliwość komendy");
+hold on
+grid on
+grid minor
+plot(linspace(1,WINDOW_NUM, WINDOW_NUM), sum_mean);
+hold off
+
+figure(3)
+title("Średnie odchylenie standardowe komendy");
+hold on
+grid on
+grid minor
+plot(linspace(1,WINDOW_NUM, WINDOW_NUM), sum_dev);
+hold off
+
+save(strcat('../databases/cmd_database_cmd_', num2str(COMMAND_NUM), '.mat'), 'sum_cog', 'sum_mean', 'sum_dev');
+
 

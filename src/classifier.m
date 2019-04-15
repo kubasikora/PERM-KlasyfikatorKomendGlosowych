2f8cf6fd71c2ec85cef453014224c8f34cf58be8
Kamil W <kamkam@onet.eu>
2019-04-15 09:27:30 +0200

    Implement command recognition in classifier and add test2.wav

--- src/classifier.m
+++ src/classifier.m
@@ -46,5 +46,46 @@ for j = 1:COMMAND_COUNT
         err_mean(j) = err_mean(j) + (sum_mean(k) - test_mean(k))^2;
         err_dev(j) = err_dev(j) + (sum_dev(k) - test_dev(k))^2;
     end
-
 end
+
+% przechowanie indeksow komend, dla ktorych blad jest najmniejszy
+eval = zeros(3,1);
+
+[min_val_cog, eval(1)] = min(err_cog);
+[min_val_mean, eval(2)] = min(err_mean);
+[min_val_dev, eval(3)] = min(err_dev);
+
+% zliczanie wystapien i wyluskanie maksymalnej liczby powtorzen
+[a,b]=hist(eval,unique(eval));
+[max_eval, ind_eval] = max(a);
+if (max_eval >= 2)
+    outcome = b(ind_eval);
+    disp(strcat('Rozpoznano komende: ', num2str(outcome)));
+else
+    disp('Nie udalo sie rozpoznac komendy!');
+end
+
+%% rysowanie wykresow
+figure(4)
+title("Średni środek ciężkości komendy");
+hold on
+grid on
+grid minor
+plot(linspace(1,WINDOW_NUM, WINDOW_NUM), test_cog);
+hold off
+
+figure(5)
+title("Średnia częstotliwość komendy");
+hold on
+grid on
+grid minor
+plot(linspace(1,WINDOW_NUM, WINDOW_NUM), test_mean);
+hold off
+
+figure(6)
+title("Średnie odchylenie standardowe komendy");
+hold on
+grid on
+grid minor
+plot(linspace(1,WINDOW_NUM, WINDOW_NUM), test_dev);
+hold off
